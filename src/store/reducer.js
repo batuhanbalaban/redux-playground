03d8bc19448fedfa6c2af9eab576856400411518
unknown <batuhan.balaban@yahoo.com>
2019-09-17 20:40:17 +0300

    redux global state updated from Component with dispatch

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -3,6 +3,31 @@ const initialState = {
 }
 
 const reducer = (state = initialState, action)=>{
+
+    switch(action.type){
+        case('INCREMENT'):
+        return {
+            ...state,
+            counter: state.counter + 1
+        }
+        case('DECREMENT'):
+        return {
+            ...state,
+            counter: state.counter - 1
+        }
+        case('ADD'):
+        return {
+            ...state,
+            counter: state.counter + action.value
+        }
+        case('SUBTRACT'):
+        return {
+            ...state,
+            counter: state.counter -  action.value
+        }
+        default:
+        break;
+    }
     return state;
 }
 
