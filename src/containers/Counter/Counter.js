64596eb7f9818e938089aa318d3cc1746fba723b
unknown <batuhan.balaban@yahoo.com>
2019-09-17 21:07:33 +0300

    Updating State Immutably

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -34,6 +34,14 @@ class Counter extends Component {
                 <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}  />
                 <CounterControl label="Add 5" clicked={() => this.props.onAddCounter(5)}  />
                 <CounterControl label="Subtract 5" clicked={() => this.props.onSubtractCounter(5)}   />
+                <hr/>
+                <button onClick = {this.props.onStoreResult}>Store Result</button>
+                <ul>
+                    {this.props.storedResults.map(strResult => (
+                        <li key={strResult.id} onClick={this.props.onDeleteResult}>{strResult.value}</li>
+                    ))}
+                    
+                </ul>
             </div>
         );
     }
@@ -42,7 +50,8 @@ class Counter extends Component {
 const mapStateToProps = state => {
 
     return {
-        ctr: state.counter
+        ctr: state.counter,
+        storedResults: state.results
     };
 }
 
@@ -51,7 +60,9 @@ const mapDispatchToProps = dispatch =>{
         onIncrementCounter: () => dispatch({type:'INCREMENT'}),
         onDecrementCounter: () => dispatch({type:'DECREMENT'}),
         onAddCounter: (val) => dispatch({type:'ADD', value:val}),
-        onSubtractCounter: (val) => dispatch({type:'SUBTRACT', value:val})
+        onSubtractCounter: (val) => dispatch({type:'SUBTRACT', value:val}),
+        onStoreResult: () => dispatch({type:'STORE_RESULT'}),
+        onDeleteResult: () => dispatch({type:'DELETE_RESULT'})
     };
 }
 
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,30 +1,39 @@
 const initialState = {
-    counter: 0
+    counter: 0,
+    results: []
 }
 
 const reducer = (state = initialState, action)=>{
 
     switch(action.type){
         case('INCREMENT'):
-        return {
-            ...state,
-            counter: state.counter + 1
-        }
+            //this is also another way of changing state immutably
+            //can use Object.assign({},state); instead of distributing properties with ...prefix
+            //remember this is not a deep clone either
+            const newState = Object.assign({},state);
+            newState.counter= state.counter + 1;
+            return newState;
         case('DECREMENT'):
-        return {
-            ...state,
-            counter: state.counter - 1
-        }
+            return {
+                ...state,
+                counter: state.counter - 1
+            }
         case('ADD'):
-        return {
-            ...state,
-            counter: state.counter + action.value
-        }
+            return {
+                ...state,
+                counter: state.counter + action.value
+            }
         case('SUBTRACT'):
-        return {
-            ...state,
-            counter: state.counter -  action.value
-        }
+            return {
+                ...state,
+                counter: state.counter -  action.value
+            }
+        case('STORE_RESULT'):
+            return {
+                ...state,
+                //concat returnes a copy of the array adding new
+                results: state.results.concat({id: new Date(), value: state.counter})
+            }
         default:
         break;
     }
